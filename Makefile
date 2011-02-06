98e3a2a35890ef49b98021c6cc458ecf494e107b
kukarN^ <fat4096@gmail.com>
2011-02-06 17:20:39 +0100

    first commit fuckers

---
+++ Makefile
@@ -0,0 +1,5 @@
+all:
+	make -C src/	
+run:
+	@bin/main
+	
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cstdio>
+
+int main(int argc, char *argv[])
+{
+ (void)argc;
+ (void)argv;		// FUUULT
+	printf("häda!!!\n");
+}
